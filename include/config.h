2673d28892fdc9100d0f7d784071be94a7c9b292
k21971 <keith.simpson1971@gmail.com>
2023-05-21 02:37:18 +0000

    Update install scripts and files for new version (6.0.8).

--- include/config.h
+++ include/config.h
@@ -228,7 +228,7 @@
  * otherwise it will be the current directory.
  */
 # ifndef HACKDIR
-#  define HACKDIR "/unnethack-6.0.7/var"
+#  define HACKDIR "/unnethack-6.0.8/var"
 # endif
 
 /*
--- install-to-chroot.sh
+++ install-to-chroot.sh
@@ -21,11 +21,11 @@ COMPRESSBIN="/bin/gzip"
 NH_GIT="/home/build/UnNetHack"
 NH_BRANCH="master"
 # HACKDIR from include/config.h; aka nethack subdir inside chroot
-NHSUBDIR="unnethack-6.0.7"
+NHSUBDIR="unnethack-6.0.8"
 # VAR_PLAYGROUND from include/unixconf.h
-NH_VAR_PLAYGROUND="/unnethack-6.0.7/var"
+NH_VAR_PLAYGROUND="/unnethack-6.0.8/var"
 # nhdat location
-NHDAT_DIR="/unnethack-6.0.7"
+NHDAT_DIR="/unnethack-6.0.8"
 # END OF CONFIG
 ##############################################################################
 
@@ -50,8 +50,8 @@ set -e
 umask 022
 
 echo "Creating inprogress and userdata directories"
-mkdir -p "$NAO_CHROOT/dgldir/inprogress-un607"
-chown "$USRGRP" "$NAO_CHROOT/dgldir/inprogress-un607"
+mkdir -p "$NAO_CHROOT/dgldir/inprogress-un608"
+chown "$USRGRP" "$NAO_CHROOT/dgldir/inprogress-un608"
 mkdir -p "$NAO_CHROOT/dgldir/extrainfo-un"
 chown "$USRGRP" "$NAO_CHROOT/dgldir/extrainfo-un"
 
