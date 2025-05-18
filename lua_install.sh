f0f259a26a946291cf40c7904a5d5de7d204a29a
k21971 <keith.simpson1971@gmail.com>
2025-05-18 23:08:02 +0000

    Specify remote/branch in lua script.

--- lua_install.sh
+++ lua_install.sh
@@ -5,7 +5,7 @@
 #git submodule add -b v5.4 https://github.com/lua/lua.git liblua
 
 # LUA setup (every time):
-(cd liblua ; git pull ; make)
+(cd liblua ; git pull origin v5.4 ; make)
 export LUA=`pwd`/liblua/lua
 export LUA_INCLUDE=-I`pwd`/liblua
 export LUA_LIB="`pwd`/liblua/liblua.a -lm"
