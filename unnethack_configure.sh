cf70518f221e58e2cee331dde2944f67556abc26
k21971 <keith.simpson1971@gmail.com>
2025-08-17 05:24:44 +0000

    Update/fix unnethack_configure.sh script.

--- unnethack_configure.sh
+++ unnethack_configure.sh
@@ -4,6 +4,8 @@ version="6.0.14"
 
 ./configure CFLAGS="-g3 -O0 -Wno-format-overflow" \
 --with-compression=gzip \
+--with-owner=games \
+--with-group=games \
 --enable-realtime-on-botl \
 --enable-score-on-botl \
 --enable-simple-mail \
@@ -13,14 +15,17 @@ version="6.0.14"
 --enable-whereis-file="/unnethack-$version/var/whereis/%n.whereis" \
 --enable-dump-file="/dgldir/userdata/%N/%n/unnethack/dumplog/%t.un.txt" \
 --enable-dump-html \
---enable-dump-html-css-embedded="/unnethack-$version/unnethack_dump.css" \
+--enable-dump-html-css-file="/unnethack-$version/unnethack_dump.css" \
+--enable-dump-html-css-embedded \
 --enable-sysconf \
 --enable-curses-graphics \
 --disable-wizmode \
 --prefix=/unnethack-$version \
---datarootdir=$prefix \
---localstatedir=$prefix/var \
---with-gamesdir=$localstatedir \
---with-sharedir=$datarootdir \
---with-unsharedir=$datarootdir \
---docdir=$datarootdir/doc
+--datarootdir=/unnethack-$version \
+--localstatedir=/unnethack-$version/var \
+--with-gamesdir=/unnethack-$version/var \
+--with-leveldir=/unnethack-$version/var \
+--with-savesdir=/unnethack-$version/var/save \
+--with-sharedir=/unnethack-$version \
+--with-unsharedir=/unnethack-$version \
+--docdir=/unnethack-$version/doc
